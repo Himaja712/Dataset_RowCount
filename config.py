4db3fcba3a4471a81b2be7ca99fb0bd019dbf380
himaja pendyala <71513466+Himaja712@users.noreply.github.com>
2025-11-21 13:15:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Power BI Configuration
+TENANT_ID     = "842e4e0e-1c63-4147-b5bc-2b8baed2f998"
+CLIENT_ID     = "c5507ed5-6034-4b81-98e5-cd6a417ba598"
+CLIENT_SECRET = "o7v8Q~MLy0v0qIS05M.oDEbDmO3tNx789cRtpcCo"
+SCOPE = "https://analysis.windows.net/powerbi/api/.default"
+
+# Dataset details
+WORKSPACE_ID  = "4455eb7b-815a-414a-b7ff-2f8960804706"
+DATASET_ID    = "64513009-3ad9-4fe5-b35c-fc6d21dec5ed"
+table_name    = "cargo_performance_data_1000"
+
+# Database details
+file_path = r"C:\Users\hpendyala\Desktop\cargo_performance_data_1000.csv"
+
+LOG_FILE = "rowcount_log.xlsx"
+SHEET_NAME = "Log"
