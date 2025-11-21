315be562869b8461051927935924a1db1a0d4bb0
himaja pendyala <71513466+Himaja712@users.noreply.github.com>
2025-11-21 13:16:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Power BI Configuration
-TENANT_ID     = "842e4e0e-1c63-4147-b5bc-2b8baed2f998"
+TENANT_ID     = ""
 CLIENT_ID     = "c5507ed5-6034-4b81-98e5-cd6a417ba598"
-CLIENT_SECRET = "o7v8Q~MLy0v0qIS05M.oDEbDmO3tNx789cRtpcCo"
+CLIENT_SECRET = ""
 SCOPE = "https://analysis.windows.net/powerbi/api/.default"
 
 # Dataset details
-WORKSPACE_ID  = "4455eb7b-815a-414a-b7ff-2f8960804706"
+WORKSPACE_ID  = ""
 DATASET_ID    = "64513009-3ad9-4fe5-b35c-fc6d21dec5ed"
 table_name    = "cargo_performance_data_1000"
 
@@ -13,4 +13,5 @@ table_name    = "cargo_performance_data_1000"
 file_path = r"C:\Users\hpendyala\Desktop\cargo_performance_data_1000.csv"
 
 LOG_FILE = "rowcount_log.xlsx"
+
 SHEET_NAME = "Log"
